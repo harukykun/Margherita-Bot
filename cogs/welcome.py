9b9a03152ce6afc17b9b1d867770c8407f03d969
Đỗ Công Huy <168851567+harukykun@users.noreply.github.com>
2026-01-10 12:53:07 +0700

    upd

---
+++ cogs/welcome.py
@@ -0,0 +1,25 @@
+import discord
+import random
+from discord.ext import commands
+
+class Welcome(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+        self.welcome_messages = [
+            "Chào mừng {member} đến với tiệm Pizza gọi pizza dứa hoặc bị doot deet <a:kcsalami:1457413393366384869> ",
+            "Rất vui được gặp bro, {member}! Hy vọng bro sẽ thích pizza dứa ở đây<:cheemthanhlich:1443896875127144448> ",
+            "Hãy cứ là chính mình nhé, {member}! Dù bro có là Fembi thì ae Pizza vẫn luôn chào đón bro <:ChisaScary:1451015932376776845> ",
+            "Này {member}, qua đây doot deet đê <a:kcsalami:1457413393366384869>",
+        ]
+
+    @commands.Cog.listener()
+    async def on_member_join(self, member):
+        channel_id = 1397176401668472842
+        channel = member.guild.get_channel(channel_id)
+        
+        if channel:
+            random_text = random.choice(self.welcome_messages)
+            content = random_text.format(member=member.mention)
+            message = await channel.send(content)
+async def setup(bot):
+    await bot.add_cog(Welcome(bot))
--- index.py
+++ index.py
@@ -3,10 +3,15 @@ import os
 import asyncio
 from discord.ext import commands
 from dotenv import load_dotenv
+
+# Load token từ file .env
 load_dotenv()
 TOKEN = os.getenv("DISCORD_TOKEN") 
+
+# Cấu hình Intents
 intents = discord.Intents.default()
-intents.message_content = True 
+intents = discord.Intents.all()
+intents.members = True
 class MyBot(commands.Bot):
     def __init__(self):
         super().__init__(
@@ -14,7 +19,9 @@ class MyBot(commands.Bot):
             intents=intents,
             help_command=None
         )
+
     async def setup_hook(self):
+        # Tự động load tất cả các file .py trong thư mục cogs
         for filename in os.listdir('./cogs'):
             if filename.endswith('.py'):
                 try:
@@ -22,19 +29,24 @@ class MyBot(commands.Bot):
                     print(f"Đã load extension: {filename}")
                 except Exception as e:
                     print(f"Không thể load {filename}: {e}")
+    
         print("Đang đồng bộ lệnh Slash Command...")
         try:
+            # Đồng bộ các lệnh app_commands (Slash Commands) với Discord
             synced = await self.tree.sync()
             print(f"Đã đồng bộ {len(synced)} lệnh.")
         except Exception as e:
             print(f"Lỗi đồng bộ lệnh: {e}")
+
     async def on_ready(self):
         print(f'Logged in as {self.user} (ID: {self.user.id})')
         print('------')
+
 async def main():
     bot = MyBot()
     async with bot:
         await bot.start(TOKEN)
+
 if __name__ == '__main__':
     if not TOKEN:
         print("Lỗi: Chưa tìm thấy DISCORD_TOKEN trong file .env")
